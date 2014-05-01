f5990d516da1e54680bc87247c127115804edf89
jerrysdesign <jerry7722@hotmail.com>
2014-05-01 15:30:04 +0800

    google map api remove

--- templates/project/source/javascripts/all.js
+++ templates/project/source/javascripts/all.js
@@ -315,65 +315,10 @@
         });
     });
 
-
-
-
   });
 
 
 })(jQuery);
-
-
-/*==Google MAP==*/
-function initialize() {
-  var image = 'images/map-marker.png';
-  var myLatlng = new google.maps.LatLng(41.850033, -87.6500523);
-  var mapOptions = {
-    zoom: 15,
-    scrollwheel: false,
-    navigationControl: false,
-    mapTypeControl: false,
-    scaleControl: false,
-    draggable: true,
-    center: myLatlng
-  };
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-  var contentString = '<div id="content">' +
-    '<div id="siteNotice">' +
-    '</div>' +
-    '<h3 id="firstHeading" class="firstHeading">CHRIS NISWANDEE</h3>' +
-    '<div id="bodyContent">' +
-    '<p>SMALLSYS INC ' +
-    '799 E DRAGRAM SUITE 5A ' +
-    '<a href="#">www.themebucket.net </a>' +
-    'TUCSON AZ 85705, USA. </p>' +
-    '</div>' +
-    '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 280
-  });
-
-
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    icon: image,
-    animation: google.maps.Animation.DROP,
-    title: 'Local Address'
-  });
-
-  google.maps.event.addListener(marker, 'click', function () {
-    infowindow.open(map, marker);
-  });
-
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
 
 /*==WOW JS==*/
 var ww = $(window).width();
